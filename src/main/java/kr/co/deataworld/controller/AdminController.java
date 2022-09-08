5f28644979d3eb25445084234cb3e632d633a626
cttc-cttc <kwy1762@naver.com>
2022-09-08 17:32:03 +0900

    관리자화면 페이지 처리2

--- src/main/java/kr/co/deataworld/controller/AdminController.java
+++ src/main/java/kr/co/deataworld/controller/AdminController.java
@@ -30,12 +30,21 @@ public class AdminController {
 	public String employeeList(Model model, int page) throws Exception {
 		logger.info("관리자화면 접속 : 구직자 회원 리스트");
 		
+		// 1페이지 미만의 번호를 넘기면 1페이지로
+		if(page < 1)
+			return "redirect:employee_list?page=1";
+		
 		PageProcess pp = new PageProcess();
 		pp.setCurrPage(page); // 현재 페이지
 		pp.setPagePerList(10); // 한 페이지에 보여줄 구직자 수
 		pp.setPagePerNavi(10); // 페이지 네비게이터에 표시할 페이지 수
 		pp.setBoardCnt(service.employeeCnt()); // 전체 구직자 수
 		
+		// 마지막 페이지를 초과한 번호를 넘기면 마지막 페이지로
+		int lastPage = pp.calcLastPage();
+		if(page > lastPage)
+			return "redirect:employee_list?page=" + lastPage;
+			
 		List<EmployeeEntity> eList = service.employeeList(pp);
 		model.addAttribute("eList", eList);
 		model.addAttribute("pp", pp);
