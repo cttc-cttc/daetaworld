44ba3e45d9e664a2c420f8679b3982acb9cd427a
cttc-cttc <kwy1762@naver.com>
2022-09-23 15:30:02 +0900

    회원가입 약관보기, 유효성 추가

--- src/main/webapp/resources/custom_js/account/register.js
+++ src/main/webapp/resources/custom_js/account/register.js
@@ -1,71 +1,9 @@
 /**
  * 구직자 회원가입, 구인자 회원가입 공용 스크립트 
  */
-let veriCheck = false;
-let nickveri = false;
-let idveri = false;
-
-$(function() {
-	$(".innerrr").on("click", "#check_all", function() {
-		var checked = $(this).is(":checked");
-		if(checked)
-			$(this).parents(".innerrr").find('input').prop("checked", true);
-		else
-			$(this).parents(".innerrr").find('input').prop("checked", false);
-	});
-
-	// 이메일 인증
-	$('#mail-Check-Btn').click(function() {
-		const email = $('#m_email').val(); // 이메일 주소값 얻어오기!
-		console.log('완성된 이메일 : ' + email); // 이메일 오는지 확인
-		const checkInput = $('.mail-check-input') // 인증번호 입력하는곳 
-		veriCheck = false;
-
-		$.ajax({
-			url : 'emailAuth',
-			data : {
-				'email' : email
-			},
-			dataType : 'json',
-			type : 'post',
-			success : function(res) {
-				alert("인증번호가 발송되었습니다.");
-				console.log(res);
-				email_auth_cd = res;
-				$('.mail-check-input').attr("disabled", false);
-			},
-			error : function(res) {
-				alert("메일 발송에 실패했습니다.");
-				console.log(res);
-			}
-		}); // end ajax
-	}); // end send eamil
-
-	// 인증번호 비교 
-	// blur -> focus가 벗어나는 경우 발생
-	$('.mail-check-input').blur(function() {
-		const inputCode = $(this).val();
-		const $resultMsg = $('#mail-check-warn');
-
-		if (inputCode == email_auth_cd) {
-			$resultMsg.html('인증번호가 일치합니다.');
-			$resultMsg.css('color', 'green');
-			$('#mail-Check-Btn').attr('disabled', true);
-			$('#userEmail1').attr('readonly', true);
-			$('#userEmail2').attr('readonly', true);
-			$('#userEmail2').attr('onFocus',
-					'this.initialSelect = this.selectedIndex');
-			$('#userEmail2').attr('onChange',
-					'this.selectedIndex = this.initialSelect');
-			$('.mail-check-input').attr("disabled", true);
-			veriCheck = true;
-		} else {
-			$resultMsg.html('인증번호가 불일치 합니다. 다시 확인해주세요!');
-			$resultMsg.css('color', 'red');
-			veriCheck = false;
-		}
-	});
-});
+let veriCheck = false; // 이메일 인증 여부
+let nickveri = false; // 닉네임 중복체크 여부
+let idveri = false; // 아이디 중복체크 여부
 
 // 주소 검색
 window.onload = function() {
@@ -80,9 +18,25 @@ window.onload = function() {
 	});
 }
 
+// 약관 전체 체크/해제
+function checkAll() {
+	const checkbox = $('input:checkbox[id="check_all"]');
+	let checked = checkbox.is(':checked');
+	if(checked)
+		checkbox.parents('.agree_check').find('input').prop('checked', true);
+	else
+		checkbox.parents('.agree_check').find('input').prop('checked', false);
+}
+
 // 닉네임 중복체크
 function m_nickCheck() {
+	if($('#m_nick').val() == '') {
+		alert('닉네임을 입력해주세요.');
+		$('#m_nick').focus();
+		return;
+	}
 	nickveri = false;
+	
 	$.ajax({
 		url : "account/m_nickChk",
 		data : {
@@ -94,12 +48,14 @@ function m_nickCheck() {
 			if (result == 1) {
 				alert('이미 등록된 닉네임입니다.');
 				console.log('불가');
-			} else if (result == 0) {
+				$('#veriChkNick').html('');
+				$('#m_nick').focus();
+			} else {
 				alert('사용 가능한 닉네임입니다.');
 				console.log('가능');
 				nickveri = true;
-			} else {
-				alert('닉네임을 입력하세요.');
+				$('#veriChkNick').html('사용가능');
+				$('#m_id').focus();
 			}
 		}
 	});
@@ -107,7 +63,13 @@ function m_nickCheck() {
 
 // 아이디 중복체크
 function m_idCheck() {
+	if($('#m_id').val() == '') {
+		alert('아이디를 입력해주세요.');
+		$('#m_id').focus();
+		return;
+	}
 	idveri = false;
+	
 	$.ajax({
 		url : "account/m_idChk",
 		data : {
@@ -119,116 +81,242 @@ function m_idCheck() {
 			if (result == 1) {
 				alert('이미 등록된 아이디입니다.');
 				console.log('불가');
-			} else if (result == 0) {
+				$('#veriChkId').html('');
+				$('#m_id').focus();
+			} else {
 				alert('사용 가능한 아이디입니다.');
 				console.log('가능');
 				idveri = true;
-			} else {
-				alert('아이디를 입력하세요.');
+				$('#veriChkId').html('사용가능');
+				$('#m_password').focus();
 			}
 		}
 	});
 }
 
-
-
-//joinform_check 함수로 유효성 검사
-function joinform_check() {
+// 이메일 인증
+$('#mail-check-btn').click(function() {
+	const email = $('#m_email').val(); // 이메일 주소값 얻어오기!
+	console.log('완성된 이메일 : ' + email); // 이메일 오는지 확인
+	if(email == '') {
+		alert('이메일을 입력해주세요.');
+		$('#m_email').focus();
+		return;
+	}
+	// 이메일 조건 : abc@abc.com 형식
+	const reg_email =/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,30})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+	if(!reg_email.test(email)) {
+		alert('이메일은 abc@abc.com 형식으로 입력해주세요.');
+		$('#m_email').focus();
+		return;
+	}
 	
-	//변수에 담아주기
-	/* var uid = document.getElementById("uid");
-	var pwd = document.getElementById("pwd");
-	var repwd = document.getElementById("repwd");
-	var uname = document.getElementById("uname");
-	var female = document.getElementById("female");
-	var male = document.getElementById("male");
-	var mobile = document.getElementById("mobile");
-	var email_id = document.getElementById("email_id");
-	var agree = document.getElementById("agree");
+	const checkInput = $('.mail-check-input') // 인증번호 입력하는곳 
+	veriCheck = false;
 
-	if (uid.value == "") { //해당 입력값이 없을 경우 같은말: if(!uid.value)
-		alert("아이디를 입력하세요.");
-		uid.focus(); //focus(): 커서가 깜빡이는 현상, blur(): 커서가 사라지는 현상
-		return false; //return: 반환하다 return false:  아무것도 반환하지 말아라 아래 코드부터 아무것도 진행하지 말것
+	$.ajax({
+		url : 'emailAuth',
+		data : {
+			'email' : email
+		},
+		dataType : 'json',
+		type : 'post',
+		success : function(res) {
+			alert("인증번호가 발송되었습니다.");
+			console.log(res);
+			email_auth_cd = res;
+			$('.mail-check-input').attr("disabled", false);
+		},
+		error : function(res) {
+			alert("메일 발송에 실패했습니다.");
+			console.log(res);
+		}
+	}); // end ajax
+}); // end send eamil
+
+// 인증번호 비교 
+// blur -> focus가 벗어나는 경우 발생
+$('.mail-check-input').blur(function() {
+	const inputCode = $(this).val();
+	const $resultMsg = $('#mail-check-warn');
+
+	if (inputCode == email_auth_cd) {
+		$resultMsg.html('인증번호가 일치합니다.');
+		$resultMsg.css('color', 'mediumseagreen');
+		$('#mail-check-btn').attr('disabled', true);
+		$('#userEmail1').attr('readonly', true);
+		$('#userEmail2').attr('readonly', true);
+		$('#userEmail2').attr('onFocus',
+				'this.initialSelect = this.selectedIndex');
+		$('#userEmail2').attr('onChange',
+				'this.selectedIndex = this.initialSelect');
+		$('.mail-check-input').attr("disabled", true);
+		veriCheck = true;
+		$('#veriChkEmail').html('인증됨');
+		$('#mail-check-btn').css('cursor', 'context-menu');
+		$('#m_email').attr("disabled", true);
+	} else {
+		$resultMsg.html('인증번호가 불일치 합니다. 다시 확인해주세요!');
+		$resultMsg.css('color', 'red');
+		veriCheck = false;
+		$('#veriChkEmail').html('');
 	}
+});
 
-	if (pwd.value == "") {
-		alert("비밀번호를 입력하세요.");
-		pwd.focus();
-		return false;
+// 유효성 검사
+function joinform_check() {
+	let agree1 = $('#agree1').is(':checked');
+	let agree2 = $('#agree2').is(':checked');
+	let agree3 = $('#agree3').is(':checked');
+	let m_nick = $('#m_nick').val();
+	let m_id = $('#m_id').val();
+	let m_password = $('#m_password').val();
+	let password2 = $('#password2').val();
+	let m_name = $('#m_name').val();
+	let m_age = $('#m_age').val();
+	let m_email = $('#m_email').val();
+	let m_phone = $('#m_phone').val();
+	let m_address1 = $('#m_address1').val();
+	let m_address2 = $('#m_address2').val();
+	
+	if(!agree1) {
+		alert('필수 약관을 체크하셔야 합니다. (만 15세 이상 여부)');
+		$('#agree1').focus();
+		return;
 	}
-
-	//비밀번호 영문자+숫자+특수조합(8~25자리 입력) 정규식
-	var pwdCheck = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
-
-	if (!pwdCheck.test(pwd.value)) {
-		alert("비밀번호는 영문자+숫자+특수문자 조합으로 8~25자리 사용해야 합니다.");
-		pwd.focus();
-		return false;
+	if(!agree2) {
+		alert('필수 약관을 체크하셔야 합니다. (서비스 이용약관 동의)');
+		$('#agree2').focus();
+		return;
 	}
-
-	if (repwd.value !== pwd.value) {
-		alert("비밀번호가 일치하지 않습니다..");
-		repwd.focus();
-		return false;
+	if(!agree3) {
+		alert('필수 약관을 체크하셔야 합니다. (개인정보 수집 및 이용 동의)');
+		$('#agree3').focus();
+		return;
 	}
-
-	if (uname.value == "") {
-		alert("이름을 입력하세요.");
-		uname.focus();
-		return false;
+	
+	if(m_nick == '') {
+		alert('닉네임을 입력해주세요.');
+		$('#m_nick').focus();
+		return;
 	}
-
-	if (!female.checked && !male.checked) { //둘다 미체크시
-		alert("성별을 선택해 주세요.");
-		female.focus();
-		return false;
+	// 닉네임 조건 : 2~8자 한글, 영문, 숫자
+	const reg_nick = /^[가-힣A-Za-z0-9]{2,8}$/;
+	if(!reg_nick.test(m_nick)) {
+		alert('닉네임은 2~8자 한글, 영문, 숫자만 사용 가능합니다.');
+		$('#m_nick').focus();
+		return;
 	}
-
-	var reg = /^[0-9]+/g; //숫자만 입력하는 정규식
-
-	if (!reg.test(mobile.value)) {
-		alert("전화번호는 숫자만 입력할 수 있습니다.");
-		mobile.focus();
-		return false;
+	
+	if(m_id == '') {
+		alert('아이디를 입력해주세요.');
+		$('#m_id').focus();
+		return;
 	}
-
-	if (email_id.value == "") {
-		alert("이메일 주소를 입력하세요.");
-		email_id.focus();
-		return false;
+	// 아이디 조건 : 6~16자 영문, 숫자
+	const reg_id = /^[A-Za-z0-9]{6,16}$/;
+	if(!reg_id.test(m_id)) {
+		alert('아이디는 6~16자 영문, 숫자만 사용 가능합니다.');
+		$('#m_id').focus();
+		return;
 	}
+	
+	if(m_password == '') {
+		alert('비밀번호를 입력해주세요.');
+		$('#m_password').focus();
+		return;
+	}
+	// 비밀번호 조건 : 영문 + 숫자 + 특수문자 8~16자리
+	const reg_pass = /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]).{8,16}$/;
+	if(!reg_pass.test(m_password)) {
+		alert('비밀번호는 8~16자 영문 + 숫자 + 특수문자의 조합으로 작성해주세요.');
+		$('#m_password').focus();
+		return;
+	}
+	
+	if(password2 == '') {
+		alert('비밀번호 확인을 입력해주세요.');
+		$('#password2').focus();
+		return;
+	}
+	
+	if(m_password != password2) {
+		alert('비밀번호가 일치하지 않습니다.');
+		$('#password2').focus();
+		return;
+	}
+	
+	if(m_name == '') {
+		alert('이름을 입력해주세요');
+		$('#m_name').focus();
+		return;
+	}
+	
+	if(m_age == '') {
+		alert('나이를 입력해주세요');
+		$('#m_age').focus();
+		return;
+	}
+	// 나이 조건 : 15이상 120이하의 숫자
+	const reg_age = /^[0-9]{2,3}$/;
+	if(!reg_age.test(m_age) || m_age < 15 || m_age > 120) {
+		alert('나이는 15이상 120이하의 숫자만 입력 가능합니다.');
+		$('#m_age').focus();
+		return;
+	}
+	
+	if(m_email == '') {
+		alert('이메일을 입력해주세요');
+		$('#m_email').focus();
+		return;
+	}
+	// 이메일 조건 : abc@abc.com 형식
+	const reg_email =/^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,30})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
+	if(!reg_email.test(m_email)) {
+		alert('이메일은 abc@abc.com 형식으로 입력해주세요.');
+		$('#m_email').focus();
+		return;
+	}
+	
+	if(m_phone == '') {
+		alert('휴대폰 번호를 입력해주세요');
+		$('#m_phone').focus();
+		return;
+	}
+	// 휴대폰 번호 조건 : 000-0000-0000
+	const reg_phone = /^\d{3}-\d{3,4}-\d{4}$/;
+	if(!reg_phone.test(m_phone)) {
+		alert('휴대폰 번호는 000-0000-0000 형식으로 입력해주세요.');
+		$('#m_phone').focus();
+		return;
+	}
+	
+	if(m_address1 == '') {
+		alert('주소를 입력해주세요');
+		$('#m_address1').focus();
+		return;
+	}
+	
+	if(m_address2 == '') {
+		alert('상세주소를 입력해주세요');
+		$('#m_address2').focus();
+		return;
+	}
+	
+	if(!nickveri) {
+		alert('닉네임 중복 여부를 확인해주세요.');
+		$('#m_nickChk').focus();
+		return;
+	}
+	
+	if(!idveri) {
+		alert('아이디 중복 여부를 확인해주세요.');
+		$('#m_idChk').focus();
+		return;
+	}
+	
+	// 이메일 인증은 패스
 
-	if (!agree1.checked) { //체크박스 미체크시
-		alert("약관 동의를 체크하세요.");
-		agree.focus();
-		return false;
-	} */
-
-	alert('가입성공');
-	//입력 값 전송
+	alert('오늘의대타 가입을 환영합니다.');
 	$('form').submit();
-}
-
-//아이디 중복체크 팝업창(현재 공백문서)
-function id_check() {
-	//window.open("팝업될 문서 경로", "팝업될 문서 이름", "옵션");
-	window.open("", "", "width=600, height=200, left=200, top=100");
-}
-
-//이메일 옵션 선택후 주소 자동 완성
-function change_email() {
-	var email_add = document.getElementById("email_add");
-	var email_sel = document.getElementById("email_sel");
-
-	//지금 골라진 옵션의 순서와 값 구하기
-	var idx = email_sel.options.selectedIndex;
-	var val = email_sel.options[idx].value;
-
-	email_add.value = val;
-}
-
-//우편번호 검색 팝업창(현재 공백문서)
-function search_address() {
-	window.open("", "b", "width=600, height=300, left=200, top=100");
 }
